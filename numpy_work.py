74e2ff4f2228e629e07a31d0166d73cb64843085
shivani gupta <shivanigupta202320@gmail.com>
2026-03-07 16:50:48 +0530

    Add basic numpy file

---
+++ numpy_work.py
@@ -0,0 +1,28 @@
+python_list=[1,2,3,4,5]
+print(python_list)
+
+import numpy as np
+numpy_array=np.array([1,2,3,4,5])
+print(numpy_array)
+
+
+one_dimensional=np.array([1,2,3,4,5])
+print(one_dimensional)
+
+two_dimensional=np.array([[1,2,3],[4,5,6]])
+print(two_dimensional)
+
+ones_array=np.ones((3,4))
+print(ones_array)
+
+filled_array=np.full((2,3),7)
+print(filled_array)
+
+#creating sequence of numbers in numpy
+sequence=np.arange(0,10,2)
+print(sequence)
+
+#creating identity matrices
+identity_matrix=np.eye(4)
+print(identity_matrix)
+
